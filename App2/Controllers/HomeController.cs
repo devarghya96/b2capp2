bd477c7febcab8de75f325543d952d5c66d8d0f2
devarghya96 <82960956+devarghya96@users.noreply.github.com>
2023-02-13 17:56:12 +0400

    App 2 Initialization
    Added Sign in flow
    Added profile edit flow
    Protect App2 from unauthenticated users
    Added SSO
    Modified Index
    Added SLO

---
+++ App2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using App2.Models;
+
+namespace App2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        if (User.Claims.Select(c => c.Type).Contains("extension_Game"))
+        {
+            return View();
+        }
+        return Redirect("/MicrosoftIdentity/Account/EditProfile");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
